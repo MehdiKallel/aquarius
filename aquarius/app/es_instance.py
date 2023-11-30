924e5e68933a7edd5087ac33825abb482a29d712
Mehdi Kallel <33198496+MehdiKallel@users.noreply.github.com>
2023-11-30 11:01:33 +0100

    remove index creation

--- aquarius/app/es_instance.py
+++ aquarius/app/es_instance.py
@@ -1,11 +1,6 @@
-#
-# Copyright 2023 Ocean Protocol Foundation
-# SPDX-License-Identifier: Apache-2.0
-#
 import logging
 import os
 import time
-
 from elasticsearch import Elasticsearch
 from elasticsearch.exceptions import NotFoundError
 from aquarius.events.util import make_did
@@ -14,7 +9,6 @@ _DB_INSTANCE = None
 
 logger = logging.getLogger(__name__)
 
-# get rid of annoying messages from https://github.com/elastic/elastic-transport-python/blob/0a3445be1723e4ef68492ee7da51d70d8c58f1d7/elastic_transport/_async_transport.py#L266
 logging.getLogger("elastic_transport.node").setLevel(logging.ERROR)
 logging.getLogger("elastic_transport.node_pool").setLevel(logging.ERROR)
 logging.getLogger("elastic_transport.transport").setLevel(logging.ERROR)
@@ -23,32 +17,26 @@ logging.getLogger("elastic_transport.transport").setLevel(logging.ERROR)
 class ElasticsearchInstance(object):
     def __init__(self):
         args = {}
-        host = os.getenv("DB_HOSTNAME", "https://localhost")
+        host = os.getenv("DB_HOSTNAME", "localhost")
         port = int(os.getenv("DB_PORT", 9200))
         username = os.getenv("DB_USERNAME", "elastic")
         password = os.getenv("DB_PASSWORD", "changeme")
-        args["http_auth"] = (username, password)
-        args["maxsize"] = 1000
-        ssl = self.str_to_bool(os.getenv("DB_SSL", "false"))
+        args["basic_auth"] = (username, password)
+        args["connections_per_node"] = 1000
+        ssl = True
         if ssl:
-            args["verify_certs"] = self.str_to_bool(
-                os.getenv("DB_VERIFY_CERTS", "false")
-            )
+            args["verify_certs"] = self.str_to_bool(os.getenv("DB_VERIFY_CERTS", "false"))
             args["ca_certs"] = os.getenv("DB_CA_CERTS", None)
             args["client_key"] = os.getenv("DB_CLIENT_KEY", None)
             args["client_cert"] = os.getenv("DB_CLIENT_CERT", None)
             args["ssl_show_warn"] = False
-        index = os.getenv("DB_INDEX", "oceandb")
-        self._index = index
-        self._did_states_index = f"{self._index}_did_states"
+        scheme = "https" if ssl else "http"
+        self._index = os.getenv("DB_INDEX", "oceandb")
         try:
-            self._es = Elasticsearch(host + ":" + str(port), **args)
-            while self._es.ping() is False:
+            self._es = Elasticsearch(f"{scheme}://{host}:{port}", **args)
+            while not self._es.ping():
                 logging.info("Trying to connect...")
                 time.sleep(5)
-
-            self._es.indices.create(index=index, ignore=400)
-            self._es.indices.create(index=self._did_states_index, ignore=400)
 
         except Exception as e:
             logging.info(f"Exception trying to connect... {e}")
@@ -91,7 +79,7 @@ class ElasticsearchInstance(object):
             body=obj,
             refresh="wait_for",
         )["_id"]
-
+    
     def read(self, resource_id):
         """Read object in elasticsearch using the resource_id.
         :param resource_id: id of the object to be read.
@@ -185,18 +173,19 @@ class ElasticsearchInstance(object):
             "tx_id": txid,
             "valid": valid,
             "error": error,
+            "doc_type": "did_state"
         }
         logger.info(f"Set did state {obj} for {did}")
         return self.es.index(
-            index=self._did_states_index,
+            index=self.db_index,
             id=did,
             body=obj,
             refresh="wait_for",
         )["_id"]
-
+    
     def read_did_state(self, did):
         """Read did index state.
         :param did
         :return: object value from elasticsearch.
         """
-        return self.es.get(index=self._did_states_index, id=did)["_source"]
+        return self.es.get(index="log-app-compute-to-data--8-w2023.42", id=did)["_source"]
